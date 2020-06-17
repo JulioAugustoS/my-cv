b11de0c843d55085cc3ce3ac5ceb39ed657b537f
Julio Augusto <39813875+JulioAugustoS@users.noreply.github.com>
2020-06-17 17:42:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,18 +12,24 @@
         from: "Ferraz de Vasconcelos, São Paulo",
         skills: {
           hard: [
-            "HTML",
-            "CSS",
+            "HTML5",
+            "CSS3",
             "JavaScript",
             "TypeScript",
             "ReactJs",
             "React Native",
             "Redux",
-            "VueJs",
-            "NativeScript",
             "GraphQL",
             "NodeJs",
+          ],
+          medium: [
+            "VueJs",
+            "NativeScript",
+            "Flutter",
+            "Objective-C",
+            "Swift",
             "Lua",
+            "Unity"
           ],
           soft: [
             "Trabalho em equipe",
@@ -34,20 +40,33 @@
           ]
         },
         academics: {
-          message: "Não deixe a faculdade atrapalhar seus estudos!"
+          message: "Não cursei ensino superior"
         },
+        socialProjects: [
+          {
+            name: "Missão Covid",
+            description: "Voluntario no desenvolvimento das plataformas do Missão Covid que liga médicos voluntários a pacientes com sintomas da Covid-19."
+          },
+          {
+            name: "Workshop Mobile Livia Teles",
+            description: "Ministrei um curso de React Native em um Workshop que teve como objetivo arrecadar doações em dinheiro em forma da inscrição para os cursos, toda a quantia arrecadada foi voltada ao tratamento de Lívia Teles, uma bebezinha que tem uma doença chamada A.M.E."
+          }
+        ],
         experiences: [
           {
             name: "RandSoft Cloud Network",
-            when: "02/2013 até 01/2015"
+            when: "02/2013 até 01/2015",
+            role: "Web Developer"
           },
           {
-            name: "Passaro Marron",
-            when: "01/2016 até 10/2018"
+            name: "Litoranea Transportes",
+            when: "01/2016 até 10/2018",
+            role: "Full Stack Developer"
           },
           {
             name: "Gok Digital",
-            when: "10/2018 até agora"
+            when: "10/2018 até agora",
+            role: "Sênior Mobile Developer"
           }
         ]
       }
